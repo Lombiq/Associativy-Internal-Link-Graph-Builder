410a117855e87c3948b0e88c1517ee86a8f2bebe
Piedone <piedone@pyrocenter.hu>
2014-06-25 23:00:28 +0200

    Refactoring dynamic pages

---
+++ Handlers/AdminPageHandler.cs
@@ -0,0 +1,22 @@
+﻿using Associativy.Administration;
+using Associativy.InternalLinkGraphBuilder.Models.Pages.Admin;
+using Orchard.ContentManagement.Handlers;
+using Piedone.HelpfulLibraries.Contents.DynamicPages;
+
+namespace Associativy.InternalLinkGraphBuilder.Handler
+{
+    public class AdminPageHandler : ContentHandler
+    {
+        protected override void Initializing(InitializingContentContext context)
+        {
+            var pageContext = context.PageContext();
+
+            if (pageContext.Group != AdministrationPageConfigs.Group) return;
+
+            if (pageContext.Page.IsPage("ManageGraph", pageContext.Group))
+            {
+                pageContext.Page.Weld(new AssociativyInternalLinkGraphManageGraphPart());
+            }
+        }
+    }
+}
